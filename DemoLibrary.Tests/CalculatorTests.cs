062ae9125255231982c4b8b32644ca9254f430b2
phyo-kyaw <phy0ky6w@gmail.com>
2021-03-06 16:18:42 +1100

    Xunit intro

---
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(4,3,7)]
+        [InlineData(21,2.25, 23.25)]
+        [InlineData(double.MaxValue, 5, double.MaxValue)]
+        public void Add_SimpleValuesShouldCalculate(double x, double y, double expected)
+        {
+            //Arrange 
+            //double expected = 5;
+
+            //Act
+            double actual = Calculator.Add(x, y);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Theory]
+        [InlineData(8, 4, 2)]
+        //[InlineData(21, 2.25, 23.25)]
+        //[InlineData(double.MaxValue, 5, double.MaxValue)]
+        public void Divide_SimpleValuesShouldCalculate(double x, double y, double expected)
+        {
+            //Arrange 
+            //double expected = 5;
+
+            //Act
+            double actual = Calculator.Divide(x, y);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        //[InlineData(8, 4, 2)]
+        //[InlineData(21, 2.25, 23.25)]
+        //[InlineData(double.MaxValue, 5, double.MaxValue)]
+        public void Divide_DividedByZero()
+        {
+            //Arrange 
+            double expected = 0;
+
+            //Act
+            double actual = Calculator.Divide(15, 0);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
